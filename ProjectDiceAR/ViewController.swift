1cc9f2250c968b123ced85e42e56a7af4dfed393
ngt-kn <nagatak@dev.local>
2018-05-16 02:08:00 -0700

    adds button and shake gesture to re-roll

--- ProjectDiceAR/ViewController.swift
+++ ProjectDiceAR/ViewController.swift
@@ -11,6 +11,8 @@ import SceneKit
 import ARKit
 
 class ViewController: UIViewController, ARSCNViewDelegate {
+    
+    var diceArray = [SCNNode]()
 
     @IBOutlet var sceneView: ARSCNView!
     
@@ -88,23 +90,37 @@ class ViewController: UIViewController, ARSCNViewDelegate {
                         hitResult.worldTransform.columns.3.y + diceNode.boundingSphere.radius,
                         hitResult.worldTransform.columns.3.z
                     )
+                    diceArray.append(diceNode)
                     sceneView.scene.rootNode.addChildNode(diceNode)
                     
-                    // random rotation for x and y axis
-                    let randomX = Float(arc4random_uniform(4) + 1) * (Float.pi/2)
-                    let randomZ = Float(arc4random_uniform(4) + 1) * (Float.pi/2)
-                    
-                    diceNode.runAction(
-                        SCNAction.rotateBy(
-                            x: CGFloat(randomX * 5),
-                            y: 0,
-                            z: CGFloat(randomZ * 5),
-                            duration: 0.5
-                        )
-                    )
+                    rollAll()
+
                 }
             }
         }
+    }
+    
+    func rollAll() {
+        if !diceArray.isEmpty {
+            for dice in diceArray {
+                roll(dice: dice)
+            }
+        }
+    }
+    
+    func roll(dice: SCNNode) {
+        // random rotation for x and y axis
+        let randomX = Float(arc4random_uniform(4) + 1) * (Float.pi/2)
+        let randomZ = Float(arc4random_uniform(4) + 1) * (Float.pi/2)
+        
+        dice.runAction(
+            SCNAction.rotateBy(
+                x: CGFloat(randomX * 5),
+                y: 0,
+                z: CGFloat(randomZ * 5),
+                duration: 0.5
+            )
+        )
     }
     
     // ARkit delegate
@@ -135,4 +151,19 @@ class ViewController: UIViewController, ARSCNViewDelegate {
             return
         }
     }
+    
+    @IBAction func rollAgain(_ sender: UIBarButtonItem) {
+        rollAll()
+    }
+    
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        rollAll()
+    }
+    
+    
+    
+    
+    
+    
+    
 }
