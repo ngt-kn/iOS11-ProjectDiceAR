1a3206e96489e353f4363e11f582988573b98374
ngt-kn <nagatak@dev.local>
2018-05-16 01:50:56 -0700

    add place dice on node

--- ProjectDiceAR/ViewController.swift
+++ ProjectDiceAR/ViewController.swift
@@ -47,15 +47,7 @@ class ViewController: UIViewController, ARSCNViewDelegate {
         
         sceneView.autoenablesDefaultLighting = true
         
-        // Create a new scene
-//        let diceScene = SCNScene(named: "art.scnassets/diceCollada.scn")!
-//
-//        if let diceNode = diceScene.rootNode.childNode(withName: "Dice", recursively: true) {
-//
-//            diceNode.position = SCNVector3(0, 0, -0.1)
-//
-//            sceneView.scene.rootNode.addChildNode(diceNode)
-//        }
+
         
 
     }
@@ -85,10 +77,18 @@ class ViewController: UIViewController, ARSCNViewDelegate {
             
             let results = sceneView.hitTest(touchLocation, types: .existingPlaneUsingExtent)
             
-            if !results.isEmpty {
-                print("Touched a plane")
-            } else {
-                print("touched somewhere else")
+            if let hitResult = results.first {
+                // Create a new scene
+                let diceScene = SCNScene(named: "art.scnassets/diceCollada.scn")!
+                
+                // place dice on plane
+                if let diceNode = diceScene.rootNode.childNode(withName: "Dice", recursively: true) {
+                    diceNode.position = SCNVector3(
+                        hitResult.worldTransform.columns.3.x,
+                        hitResult.worldTransform.columns.3.y + diceNode.boundingSphere.radius,
+                        hitResult.worldTransform.columns.3.z)
+                    sceneView.scene.rootNode.addChildNode(diceNode)
+                }
             }
         }
     }
